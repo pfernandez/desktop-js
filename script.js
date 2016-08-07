618c0f1393631dda296a85746d75844fb623de93
Paul Fernandez <paul4nandez@gmail.com>
2016-08-06 20:44:30 -0700

    Added style.css and script.js.

---
+++ script.js
@@ -0,0 +1,34 @@
+(function() {
+
+
+  // Demo: Drag-n-drop file path.
+  var finder = document.getElementById('finder');
+  finder.ondragover = function (e) {
+    e.preventDefault();
+    this.style.background = '#ccc';
+  };
+  finder.ondragleave = finder.ondragend = function () {
+    this.style.background = 'inherit';
+  };
+  finder.ondrop = function (e) {
+  alert('dropped');
+    e.preventDefault();
+    this.style.background = 'inherit';
+    var file = e.dataTransfer.files[0];
+    console.log('The file you dropped is', file.path);
+  };
+  
+  
+  // Demo: Embedded web page.
+  window.onload = function() {
+    var webview = document.getElementById('webview');
+    var loader = document.querySelector('.loader');
+    var loadstart = loadstop = function() {
+      loader.classList.toggle('hide');
+      webview.classList.toggle('hide');
+    }
+    webview.addEventListener('did-start-loading', loadstart);
+    webview.addEventListener('did-stop-loading', loadstop);
+  }
+  
+})();
